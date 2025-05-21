2173e5ae5efbd1da390f7115f0b212bc2fc6f35d
Tanvir Ahmed <shkhtanvirahmed@gmail.com>
2025-05-21 19:47:59 +0600

    Done: Struct & Interface

--- cmd/struct_interfaces/main.go
+++ cmd/struct_interfaces/main.go
@@ -20,6 +20,38 @@ func (e house) helloFromOwner() string {
 	return "Owner " + e.ownerInfo.name + " says hello to you!!"
 }
 
+type gasEngine struct {
+	mpg     int
+	gallons int
+}
+
+type electricEngine struct {
+	mpkwh int
+	kwh   int
+}
+
+func (e gasEngine) milesLeft() int {
+	return e.gallons * e.mpg
+}
+
+func (e electricEngine) milesLeft() int {
+	return e.kwh * e.mpkwh
+}
+
+// Interface
+type engine interface {
+	milesLeft() int
+}
+
+// func canMakeIt(e gasEngine, miles int) {
+func canMakeIt(e engine, miles int) {
+	if miles < e.milesLeft() {
+		fmt.Println("You can make it there!")
+	} else {
+		fmt.Println("Need to fuel up first!")
+	}
+}
+
 func main() {
 	fmt.Println("Struct & Interface: ")
 
@@ -38,4 +70,13 @@ func main() {
 	}{12, 10} // anonymous struct -> must have to declare and initialize at the same time/place and obviously not reuseable.
 
 	fmt.Println(myEngine.mph, " -> ", myEngine.price)
+
+	// -----------------------------
+	var engGas gasEngine = gasEngine{25, 15}
+	var engElec electricEngine = electricEngine{20, 10}
+
+	canMakeIt(engGas, 5) // Generic function, works on both structure. Thanks to Interface!
+	canMakeIt(engElec, 5)
+	canMakeIt(engGas, 500)
+	canMakeIt(engElec, 500)
 }
